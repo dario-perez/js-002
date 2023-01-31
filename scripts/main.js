2d3fd6521d597beee3647b8e517862064b64fd19
Dario Perez <darioperez.2105@gmail.com>
2023-01-31 13:53:58 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const birthday = new Date('June 21 1988');
+const favBook = {
+  título: 'La llamada de Cthulhu',
+  autor: 'Howard Phillip Lovecraft',
+  fecha: 1928,
+  url: 'https://www.amazon.com/-/es/H-P-Lovecraft/dp/1503096475',
+};
+
+const person = ['Dario', 34, true, birthday, favBook];
+
+console.log(person);
